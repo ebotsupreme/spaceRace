259981f1932acda2346d9c2c7d8f6089c0b70727
Eddie Jung <ejung716@gmail.com>
2021-08-24 14:27:47 -0700

    Stop debris once player dies.

--- Project17/GameScene.swift
+++ Project17/GameScene.swift
@@ -87,6 +87,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             
             if !isGameOver {
                 score += 1
+            } else {
+                gameTimer?.invalidate()
             }
         }
     }
