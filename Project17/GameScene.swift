e5ff829d16426f3b54305e4487126ebbe4eb04e3
Eddie Jung <ejung716@gmail.com>
2021-08-24 09:48:04 -0700

    initial commit.

---
+++ Project17/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  Project17
+//
+//  Created by Eddie Jung on 8/24/21.
+//
+
+import SpriteKit
+
+
+class GameScene: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
